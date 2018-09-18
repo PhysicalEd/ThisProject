1f25199e6775a7666c3cff3debaa328391d7caa7
Ed <Ed@COMPUTER>
2018-09-18 23:33:40 +1200

    CV update

--- CV/Controllers/HomeController.cs
+++ CV/Controllers/HomeController.cs
@@ -23,8 +23,33 @@ namespace CV.Controllers
                 switch (model.EmployerID)
                 {
                     case "0ED6E64A-EECD-48FC-ADF2-FB463E103D95":
-                        model.EmployerName = "Ed";
+                        model.EmployerName = "EdenCo";
                         model.EmployerCompany = "EdenCo";
+                        model.IsValidated = true;
+                        break;
+                    case "85fd0790-42e1-46f7-a3e8-0429449a5624":
+                        model.EmployerName = "Komari Enterprise";
+                        model.EmployerCompany = "Komari Enterprise";
+                        model.IsValidated = true;
+                        break;
+                    case "81b52b03-b026-4444-a576-b5c8df02f3a2":
+                        model.EmployerName = "Xero";
+                        model.EmployerCompany = "Xero";
+                        model.IsValidated = true;
+                        break;
+                    case "0ec513b8-a6d0-4c78-bd0a-c68b7dbf9046":
+                        model.EmployerName = "Lala Constructions";
+                        model.EmployerCompany = "Lala Constructions";
+                        model.IsValidated = true;
+                        break;
+                    case "93b7320f-21d4-4a95-8bea-8e74f42e3f90":
+                        model.EmployerName = "Ward and Family LTD";
+                        model.EmployerCompany = "Ward and Family LTD";
+                        model.IsValidated = true;
+                        break;
+                    case "32af428d-4140-4e32-afb8-ba3bfd4e3948":
+                        model.EmployerName = "Jad's Dad";
+                        model.EmployerCompany = "Jad's Dad";
                         model.IsValidated = true;
                         break;
                 }
