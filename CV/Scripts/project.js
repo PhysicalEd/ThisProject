c28422c01901d78817e115deef98a2e27d87fd52
Ed <Ed@COMPUTER>
2018-09-18 16:46:31 +1200

    Update GitHub sources

---
+++ CV/Scripts/project.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    let project = new Vue({
+        el: '#ProjectInstance',
+      methods: {
+            LoadGitHub: function (projectName) { LoadGitHub(projectName) }
+        }
+    });
+});
+
+function LoadGitHub(projectName) {
+    window.open("https://github.com/PhysicalEd/" + projectName);
+
+}
